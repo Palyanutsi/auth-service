4a381a8851a5772aa0f4034a9ba3fddaeaf2c50e
losimen <koval.pavlo03@gmail.com>
2024-03-03 17:37:51 +0200

    remove check func

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -187,15 +187,6 @@ export class AuthController {
   }
 
   @Public()
-  @Get('/check')
-  public async check() {
-    console.log('hello from check');
-    await this.authService.check();
-    console.log('end request');
-    return 'hello world';
-  }
-
-  @Public()
   @Post('/forgot-password')
   @HttpCode(HttpStatus.OK)
   @ApiOkResponse({
